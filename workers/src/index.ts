563428f9262ae012a31d9c4aa601c67928f22bc4
Arvind <aruparekh@gmail.com>
2024-08-24 04:28:02 +0530

    init: CF KV Stores

--- workers/src/index.ts
+++ workers/src/index.ts
@@ -4,12 +4,14 @@ import { stream } from "hono/streaming";
 
 type Bindings = {
    AI: Ai;
+   DB: KVNamespace;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
 
 export interface Env {
    AI: Ai;
+   DB: KVNamespace;
 }
 
 app.use(cors());
@@ -18,6 +20,7 @@ app.get("/", (c) => {
    return c.text("Hello Hono!");
 });
 
+// Get response from AI model
 app.post("/", async (c) => {
    const body = await c.req.json();
    console.log(body);
@@ -37,7 +40,7 @@ app.post("/", async (c) => {
       messages: [
          {
             role: "system",
-            content: `You are a GitHub Profile chat system. Users will send you their github profile data and ask you questions regarding it. Your job is to reference that memory and always answer in the context of that memory. You have to answer the questions the users ask from that memory. The memory will be in the form of json data. If you cannot find the information in the memory, let the user know.
+            content: `You are a GitHub Profile chat system. Users will send you their github profile data and ask you questions regarding it. Your job is to reference that memory and always answer in the context of that memory. You have to answer the questions the users ask from that memory. The memory will be in the form of json data. If you cannot find the information in the memory, let the user know. Also, provide all the information in a human-readable format.
             
             Following is your memory: ${memoryData}`,
          },
@@ -59,5 +62,10 @@ app.post("/", async (c) => {
       //  }
    );
 });
+
+// to put the data fetched from github into a KV store
+app.post('/updateDb', async (c)=> {
+
+})
 
 export default app;
