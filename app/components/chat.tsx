f8da4bda14b006db6acf9ffcb0bbb0a50be1de4a
Arvind <aruparekh@gmail.com>
2024-09-08 16:59:40 +0530

    another deployment fix

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1,31 +1,31 @@
-"use client";
+// "use client";
 
-import { dataContextAtom } from "@/atoms/atoms";
-import axios, { AxiosResponse } from "axios";
-import { useRecoilValue } from "recoil";
+// import { dataContextAtom } from "@/atoms/atoms";
+// import axios, { AxiosResponse } from "axios";
+// import { useRecoilValue } from "recoil";
 
-export default function Chat() {
-   const data = useRecoilValue(dataContextAtom);
+// export default function Chat() {
+//    const data = useRecoilValue(dataContextAtom);
 
-   async function handleClick() {
-      const res = await axios.post(
-         // "https://temp-workers.aruparekh2.workers.dev",
-         "http://localhost:8787",
-         {
-            memoryData: data,
-            prompt:
-               "What is the bio of the user and tell me something unique about him.",
-         }
-      );
+//    async function handleClick() {
+//       const res = await axios.post(
+//          // "https://temp-workers.aruparekh2.workers.dev",
+//          "http://localhost:8787",
+//          {
+//             memoryData: data,
+//             prompt:
+//                "What is the bio of the user and tell me something unique about him.",
+//          }
+//       );
 
-      console.log(res);
-   }
+//       console.log(res);
+//    }
 
-   return (
-      <div>
-         <p>{data.user.bio}</p>
+//    return (
+//       <div>
+//          <p>{data.user.bio}</p>
 
-         <button onClick={handleClick}>Click here</button>
-      </div>
-   );
-}
+//          <button onClick={handleClick}>Click here</button>
+//       </div>
+//    );
+// }
